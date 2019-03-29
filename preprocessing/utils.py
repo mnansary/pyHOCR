855052bdaf6e542e202db86618a4b50d8f9ca3d5
silentassasin0111 <silentassasin0111@gmail.com>
2019-03-29 19:26:07 +0600

    <ISSUE>Bin

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -12,6 +12,7 @@ class Preprocessor(object):
         self.__train_data_path=os.path.join(self.__database_path,train_data_identifier)
     
     def __load_dataset(self,path):
+        print('#    Loading Data from: {}'.format(path))
         data_bunch=load_files(path)
         file_names=np.array(data_bunch['filenames'])
         targets=np.array(data_bunch['target'])
@@ -19,27 +20,35 @@ class Preprocessor(object):
         return file_names,classes
 
     def __validation_test_train_data(self):
+        print('#    Extracting Test Train Validation data')
         self.__test_files,self.__test_classes=self.__load_dataset(self.__test_data_path)
         self.__model_files,self.__model_classes=self.__load_dataset(self.__train_data_path)
         self.__train_files,self.__validation_files,self.train_classes,self.validation_classes=train_test_split(self.__model_files,self.__model_classes,test_size=0.2,stratify=self.__model_classes)
     
-    def __convert_file_to_tensor(self,file_name):
+    
+    def __convert_file_to_tensor(self,file_name,binarize=True):
         img=load_img(file_name,color_mode = "grayscale",target_size=self.__resize_dim)
         arr=img_to_array(img)
+        if binarize:
+            arr[arr>0]=2
+            arr[arr==0]=1
+            arr[arr==2]=0
         tensor=np.expand_dims(arr,axis=0)
         return tensor
-    
+
     def __files_to_tensors(self,file_names):
         list_of_tensors = [self.__convert_file_to_tensor(file_name) for file_name in file_names]
         tensors=np.vstack(list_of_tensors)
         return tensors
 
     def __load_tensors(self):
-        self.train_tensors = self.__files_to_tensors(self.__train_files).astype('float32')/255
-        self.validation_tensors = self.__files_to_tensors(self.__validation_files).astype('float32')/255
-        self.test_tensors = self.__files_to_tensors(self.__test_files).astype('float32')/255
+        print('#    Loading Tensors')
+        self.train_tensors = self.__files_to_tensors(self.__train_files)
+        self.validation_tensors = self.__files_to_tensors(self.__validation_files)
+        self.test_tensors = self.__files_to_tensors(self.__test_files)
     
     def preprocess_data(self):
+        print('#    Preprocessing Data')
         self.__validation_test_train_data()
         self.__load_tensors()
         return [self.train_tensors,self.train_classes,self.validation_tensors,self.validation_classes]
--- train.py
+++ train.py
@@ -11,7 +11,7 @@ parser.add_argument("datafolder", help="/path/to/Data/Folder")
 args = parser.parse_args()
 database_path=args.datafolder
 
-model_name='denseNet50Default.hdf5'
+model_name='denseNet.hdf5'
 
 data_shape=(32,32,1)
 
@@ -20,18 +20,17 @@ def preprocess(database_path):
     data=preprocObj.preprocess_data()    
     return data
 
-def train_data(data_shape,model_name,data,epochs=250,batch_size=32,):
-    model=DenseNet(input_shape=data_shape,nb_classes=50,dropout_rate=0.2,compression=0.5)
+def train_data(data_shape,model_name,data,epochs=250,batch_size=32):
+    model=DenseNet(input_shape=data_shape,nb_classes=50,dense_blocks=1,depth=15,dropout_rate=0.2,compression=0.5)
     model.summary()
     model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
     epochs = epochs
     batch_size = batch_size
-    checkpoint = ModelCheckpoint(filepath=model_name, verbose=1, save_best_only=True)
+    checkpoint = ModelCheckpoint(filepath='saved_models/{}'.format(model_name), verbose=1, save_best_only=True)
     train_tensors=data[0]
     train_classes=data[1]
     validation_tensors=data[2]
     validation_classes=data[3]
-    
     model.fit(train_tensors, train_classes,validation_data=(validation_tensors, validation_classes),epochs=epochs, batch_size=batch_size, callbacks=[checkpoint], verbose=1)
 
 if __name__ == "__main__":
