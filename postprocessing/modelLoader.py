fffbe95f655a55b28a51ec470b7c9224fdf98d40
silentassasin0111 <silentassasin0111@gmail.com>
2019-04-11 01:38:41 +0600

    <ISSUE-SOLVE>Testing data

--- postprocessing/modelLoader.py
+++ postprocessing/modelLoader.py
@@ -5,6 +5,7 @@
 from __future__ import print_function
 from termcolor import colored
 
+import numpy as np 
 from keras.models import load_model
 from preprocessing.utils import Preprocessor
 from sklearn import metrics
@@ -32,7 +33,13 @@ class PostProcessor(object):
         test_tensors=preprocObj.test_tensors	   
         test_classes=preprocObj.test_classes
         
-        predictions = [np.argmax(self.model.predict(np.expand_dims(tensor,axis=0))) for tensor in test_tensors]	    
-        ground_truth = [np.argmax(truth_value) for truth_value in test_classes]	    
+        print(colored('# Generating Predictions','blue'))
+        predictions = [np.argmax(self.model.predict(np.expand_dims(tensor,axis=0))) for tensor in test_tensors]
+        
+        print(colored('# Getting Ground Truth','blue'))	    
+        ground_truth = [np.argmax(truth_value) for truth_value in test_classes]
+        
+        print(colored('# Calculating Accuracy','blue'))	    
+        
         prediction_accuracy = 100* metrics.f1_score(ground_truth,predictions, average = 'micro')	   
         print(colored('Test data Prediction Accuracy [F1 accuracy]: {}'.format(prediction_accuracy),'green'))
