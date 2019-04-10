d87906ec26f759b10162821bf9e827e780a324b1
silentassasin0111 <silentassasin0111@gmail.com>
2019-04-11 01:27:25 +0600

    <UPDATE>Testing

--- postprocessing/modelLoader.py
+++ postprocessing/modelLoader.py
@@ -6,14 +6,33 @@ from __future__ import print_function
 from termcolor import colored
 
 from keras.models import load_model
+from preprocessing.utils import Preprocessor
+from sklearn import metrics
 
 class PostProcessor(object):
-    def __init__(self,model_path=None):
-        if model_path:
-            self.model=load_model(model_path)
-    
-    def test_model(self):
-        st="ভাল"
-        print(st)
-
+    def __init__(self,model_path):
         
+        self.model=load_model(model_path)
+        self.symbol_list=[
+                        'অ','আ','ই','ঈ','উ',
+                        'ঊ','ঋ','এ','ঐ','ও',
+                        'ঔ','ক','খ','গ','ঘ',
+                        'ঙ','চ','ছ','জ','ঝ',
+                        'ঞ','ট','ঠ','ড','ঢ',
+                        'ণ','ত','থ','দ','ধ',
+                        'ন','প','ফ','ব','ভ',
+                        'ম','য','র','ল','শ',
+                        'ষ','স','হ','ড়','ঢ়',
+                        'য়','ৎ','ং','ঃ','ঁ'
+                        ]                        # 'ঁ' not print able
+        
+    def test_model(self,database_path):
+        preprocObj=Preprocessor(database_path=database_path)
+        preprocObj.preprocess_data(return_flag=False)    	       
+        test_tensors=preprocObj.test_tensors	   
+        test_classes=preprocObj.test_classes
+        
+        predictions = [np.argmax(self.model.predict(np.expand_dims(tensor,axis=0))) for tensor in test_tensors]	    
+        ground_truth = [np.argmax(truth_value) for truth_value in test_classes]	    
+        prediction_accuracy = 100* metrics.f1_score(ground_truth,predictions, average = 'micro')	   
+        print(colored('Test data Prediction Accuracy [F1 accuracy]: {}'.format(prediction_accuracy),'green'))
--- test.py
+++ test.py
@@ -7,10 +7,19 @@ from __future__ import print_function
 from termcolor import colored
 
 from postprocessing.modelLoader import PostProcessor
+import argparse
 
-def test():
-    obj=PostProcessor()
-    obj.test_model()
+parser = argparse.ArgumentParser(description='Bangla OCR 50 class Alphabet DenseNet Model')
+parser.add_argument("datafolder", help="/path/to/Data/Folder")
+parser.add_argument("model_path",help="/path/to/test/Model.hdf5")
+
+args = parser.parse_args()
+database_path=args.datafolder
+model_path=args.model_path
+
+def test(model_path,database_path):
+    obj=PostProcessor(model_path)
+    obj.test_model(database_path)
 
 if __name__=='__main__':
-    test()
+    test(model_path,database_path)
